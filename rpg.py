52c43df6a0e35074a93764560a8e4687b7934af1
Aearhart-Dominican <austin.earhart55@gmail.com>
2024-09-28 15:04:41 -0700

    Started RPG Game

---
+++ rpg.py
@@ -0,0 +1,36 @@
+import pygame as py
+import random
+
+py.init()
+
+clock = py.time.Clock()
+FPS = 60
+
+# Constants
+SCREEN_WIDTH = 700
+SCREEN_HEIGHT = 700
+
+screen = py.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+py.display.set_caption("RPG Game")
+
+BLACK = ( 0, 0, 0)
+WHITE = ( 255, 255, 255)
+RED = ( 173, 5, 5)
+BLUE = (26, 0, 196)
+GREEN = (28, 181, 7)
+PURPLE = (133, 3, 150)
+
+# Variables
+
+
+# Classes
+
+
+# Functions
+
+
+running = True
+while running:
+
+    clock.tick(FPS)
+
